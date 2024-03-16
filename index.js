25bd04f663901cb0c0f2de8d82979bbe284a425a
pallavi <pmdhakne2002@gmail.com>
2024-03-16 18:34:05 +0530

    Database connected

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import Express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = Express();
 const port = 3000;
 app.get("/", (req, res) => {
   res.send("hello welcome to the TO-do list project");
 });
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Database connected succesfully");
+  } catch (err) {
+    console.log("Error occurs while connecting to MONGODB", err);
+  }
+}
+connectToDatabase(); //function call
 app.listen(port, () => {
   console.log(`Server is running on this port ${port}`);
 });
